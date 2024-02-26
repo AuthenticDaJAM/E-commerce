155e08c96431e3a04ee3edcb1ce0e77c7cbdab92
AuthenticDaJAM <zekonilla@gmail.com>
2024-02-26 08:00:45 -0600

    Api PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<php>
+# Select an API and endpoint to get started.
+
+use Square\SquareClient;
+
+$config = [
+    'accessToken' => 'EAAAl-JOx8LoptZpMpwdr_D2kJnR_3iBMaYwk0X9oJtdR2R2sKdPtS99Xl_vSQii',
+    'environment' => 'sandbox'
+];
+$client = new SquareClient($config);
+<php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba API Square</title>
+</head>
+<body>
+    
+</body>
+</html>

4d1ae5b97deec774cc75be79d265ec6286fa7f1b
AuthenticDaJAM <zekonilla@gmail.com>
2024-03-04 18:06:57 -0600

    Se instalaron las librerías 'dotenv' y 'express' para leer las credenciales en 'cred.env', ahi está la API de Square, las librerías de '@square/square' siguen sin aparecer, por lo que aparece error y no se soluciona instalando las 16 librerías catalogadas como 'square', solo gasta recursos (6k+ archivos)

---
+++ Square_DOCS/server.js
@@ -0,0 +1,55 @@
+require('dotenv').config(); 
+
+const express = require('express'); 
+
+const { Client, Environment } = require('@square/square'); 
+
+ 
+
+const app = express(); 
+
+const port = 3000; 
+
+// SQUARE_ACCESS_TOKEN="EAAAl37vnzC3kDwe2pEu40pDPsuwNe-l1YxEe-kqIV8OXBU2CA-251s722Y6ctN5";
+
+// Configura el cliente de Square con tu token de acceso 
+
+const squareClient = new Client({ 
+
+  accessToken: process.env.SQUARE_ACCESS_TOKEN, 
+
+  environment: Environment.Sandbox, // Cambia a Environment.Production para producción 
+
+}); 
+
+ 
+
+app.use(express.static('public')); // Sirve archivos estáticos desde la carpeta 'public' 
+
+ 
+
+// Ruta para verificar la conexión a Square 
+
+app.get('/check-connection', async (req, res) => { 
+
+  try { 
+
+    const response = await squareClient.customersApi.listCustomers(); 
+
+    res.json({ success: true, message: 'Conexión exitosa con la API de Square.' }); 
+
+  } catch (error) { 
+
+    res.json({ success: false, message: 'Falló la conexión con la API de Square.' }); 
+
+  } 
+
+}); 
+
+ 
+
+app.listen(port, () => { 
+
+  console.log(`Servidor escuchando en http://localhost:${port}`); 
+
+}); 
